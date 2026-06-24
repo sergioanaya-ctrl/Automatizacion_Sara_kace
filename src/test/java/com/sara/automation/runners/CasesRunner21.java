1e0434e4bd9760e690194767bb1d0b89574868ff
sergio.anaya <Sheyo_0129>
2026-06-24 11:06:19 -0500

    flujo de Reclamaciones

--- src/test/java/com/sara/automation/runners/CasesRunner21.java
+++ src/test/java/com/sara/automation/runners/CasesRunner21.java
@@ -1,6 +1,5 @@
 package com.sara.automation.runners;
 
-import com.sara.automation.utils.RunnerContext;
 import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
